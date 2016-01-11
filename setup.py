d6021e48185051e8eb676984cfa4b33cbfd5c6b4
Marcin Bachry <hegel666@gmail.com>
2016-01-11 23:12:43 +0100

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='exxo',
-    version='0.0.2',
+    version='0.0.3',
     description="Build portable Python apps",
     long_description=readme,
     author="Marcin Bachry",
