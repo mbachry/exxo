60a4253d2faac2fe9f7bf7ca200c687a415b9873
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2023-01-26 12:13:26 -0600

    Adding tarfile member sanitization to extractall() (#13)

--- exxo/bootstrap.py
+++ exxo/bootstrap.py
@@ -73,7 +73,26 @@ def download_and_unpack(url, dst_path, dst_dir):
     try:
         print('unpacking', dst_path)
         with tarfile.open(str(dst_path)) as tar:
-            tar.extractall(str(dst_dir))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, str(dst_dir))
     finally:
         dst_path.unlink()
 
